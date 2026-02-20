a99a75c70c5b2fb69a4915e9db0108ab865dc1d1
Riven <supa7306@gmail.com>
2026-02-20 23:24:04 +0300

    Add build output

--- .svelte-kit/output/client/_app/immutable/entry/start.BdQZv7t6.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/up6IS91Z.js";export{o as load_css,r as start};
--- .svelte-kit/output/server/manifest-full.js
+++ .svelte-kit/output/server/manifest-full.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set([]),
 	mimeTypes: {},
 	_: {
-		client: {start:"_app/immutable/entry/start.BdQZv7t6.js",app:"_app/immutable/entry/app.B2boRmEB.js",imports:["_app/immutable/entry/start.BdQZv7t6.js","_app/immutable/chunks/up6IS91Z.js","_app/immutable/chunks/BCmFIbmc.js","_app/immutable/chunks/Cf9gXw06.js","_app/immutable/chunks/BrvNypCE.js","_app/immutable/chunks/CaN-eVNQ.js","_app/immutable/entry/app.B2boRmEB.js","_app/immutable/chunks/BCmFIbmc.js","_app/immutable/chunks/Cf9gXw06.js","_app/immutable/chunks/BrvNypCE.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.DOTrNFRQ.js",app:"_app/immutable/entry/app.CwblRxLv.js",imports:["_app/immutable/entry/start.DOTrNFRQ.js","_app/immutable/chunks/XlOkJ5l1.js","_app/immutable/chunks/BCmFIbmc.js","_app/immutable/chunks/Cf9gXw06.js","_app/immutable/chunks/BrvNypCE.js","_app/immutable/chunks/CaN-eVNQ.js","_app/immutable/entry/app.CwblRxLv.js","_app/immutable/chunks/BCmFIbmc.js","_app/immutable/chunks/Cf9gXw06.js","_app/immutable/chunks/BrvNypCE.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
